3299b04e8ef2ab06c2166656d324c8259441e3dd
Shubham-kimtani <32139112+Shubham-kimtani@users.noreply.github.com>
2023-10-24 16:14:12 +0530

    Add files via upload

---
+++ backend-setup/main.tf
@@ -0,0 +1,34 @@
+# provider "aws" {
+#     region = "us-east-2"
+# }
+
+# resource "aws_s3_bucket" "terraform_state_bucket" {
+#     bucket = "tf-state-bucket-for-api-gateway"
+    
+#     versioning {
+#         enabled = true
+#     }
+
+#     lifecycle {
+#       prevent_destroy = true
+#     }
+
+#     server_side_encryption_configuration {
+#     rule {
+#       apply_server_side_encryption_by_default {
+#         sse_algorithm = "AES256"
+#       }
+#     }
+#   }
+# }
+
+# resource "aws_dynamodb_table" "terraform_locks_for_api_gateway" {
+#     name = "tf_lock_api_gateway"
+#     billing_mode = "PAY_PER_REQUEST"
+#     hash_key = "LockID"
+
+#     attribute {
+#         name = "LockID"
+#         type = "S"
+#     }
+# }
